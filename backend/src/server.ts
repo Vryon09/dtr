57bde09c83502ad43dace44aa635528ff0e4395e
Vryon Antonio <vryonantonio@gmail.com>
2026-09-20 21:09:11 +0800

    backend origins explicit declaration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://hootiy.netlify.app"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
